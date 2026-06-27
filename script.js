c6f3175ff6fffdf86285644db11b6e05b9f1d756
atefdalia38-wq <atefdalia38@gmail.com>
2026-06-28 00:39:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function showMessage() {
+
     const surprise = document.getElementById("surprise");
+    const btn = document.querySelector("button");
+
+    btn.classList.add("shake");
+
+    setTimeout(() => {
+        btn.classList.remove("shake");
+    }, 700);
 
     if (surprise.style.display === "block") {
         surprise.style.display = "none";
     } else {
         surprise.style.display = "block";
-        surprise.scrollIntoView({
-            behavior: "smooth"
-        });
     }
+
 }
